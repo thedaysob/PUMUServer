fe08f9a464317a52abfa8c1ef2ec1471e508dbc4
Daeseob Lim <thedaysob@gmail.com>
2019-08-21 15:21:02 -0400

    Incorporated MongoDB and basic post and get

--- server.js
+++ server.js
@@ -1,22 +1,50 @@
-var mysql = require('mysql');
+const express = require("express");
+const BodyParser = require("body-parser");
 
-var con = mysql.createConnection({
-	host: "localhost",
-	user: "daeseob.lim@tufts.edu",
-	password: "Trackie400"
+var app = express();
+app.use(BodyParser.json());
+app.use(BodyParser.urlencoded({ extended: true }));
+
+const mongo = require('mongodb');
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://Daeseob:trackie400@pickupmeetup-b0afy.mongodb.net/test?retryWrites=true&w=majority";
+
+const client = new MongoClient(uri, { 
+	useNewUrlParser: true,
+	useUnifiedTopology: true
 });
 
-con.connect(function(err) {
-	if (err) throw err;
-	console.log("Connected");
+var database, collection;
 
-	con.connect(function(err) {
+app.get("/Locations", (req, res) => {
+	collection.find({}).toArray((error, result) => {
+		if (error) {
+			return response.status(500).send(error);
+		}
+		reponse.send(result);
+	});
+});
+
+app.post("/Location", (req, res) => {
+	collection.insert(req.body, (error, result) => {
+		if (error) {
+			return response.status(500).send(error);
+		}
+		response.send(result.result);
+	})
+})
+
+app.listen(3000, () => {
+	client.connect(function(err, db) {
 		if (err) throw err;
-		console.log("Connected!");
-		
-		con.query("CREATE DATABASE mydb", function (err, result) {
+		console.log("Connection Successful");
+
+		database = db.db("PickUpDatabase");
+		console.log("Database created PickUpDatabase");
+
+		collection = database.createCollection('Locations',  function(err, collection) {
 			if (err) throw err;
-			console.log("Database created");
+			console.log("Collection created");
 		});
 	});
 });
